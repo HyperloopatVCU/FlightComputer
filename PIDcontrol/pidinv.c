793b0107ecdbff78934c212df1c59652fa63c67a
Nabbo454 <jonathan.b.odell@gmail.com>
2017-10-30 16:22:51 -0400

    Made datatypes a little more buoyant.

--- PIDcontrol/pidinv.c
+++ PIDcontrol/pidinv.c
@@ -2,11 +2,10 @@
 
 struct pidprops prop_default = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};	//  Define initial positions of default properties.
 
-double pidder(PID *props, double input, double dt) {
-	double de = props->sp - input;		//  Specify "error".
-	props->igral += de * dt;		//  Produce next integral step.
-	double output = (props->Kp * de) + (props->Ki * props->igral) +
-		(props->Kd * (de - props->pe) / dt); //  Calculate PID output.
-	props->pe = de;		//  Update past 'error'.
+float pidder(PID *props, float input, float dt) {
+	float de = props->sp - input;			//  Specify 'error'.
+	props->igral += de * dt;			//  Produce next integral step.
+	float output = (props->Kp * de) + (props->Ki * props->igral) + (props->Kd * (de - props->pe) / dt); //  Calculate PID output.
+	props->pe = de;					//  Update past 'error'.
 	return output;
 }
--- PIDcontrol/pidinv.h
+++ PIDcontrol/pidinv.h
@@ -1,19 +1,20 @@
 #ifndef PIDINV_H
 #define PIDINV_H
 
-struct pidprops {
-	double Kp;
-	double Ki;
-	double Kd;
-	double sp;
-	double pe;
-	double igral;
+struct pidprops {	//  PID properties structure
+	float Kp;	//  Proportional constant
+	float Ki;	//  Integral constant
+	float Kd;	//  Derivative constant
+	float sp;	//  Setpoint (target value)
+
+	float pe;	//  Previous ;error'
+	float igral;	//  Previous integral value
 };
 
 typedef struct pidprops PID;
 
 extern PID prop_default;
 
-double pidder(PID *props, double input, double dt);
+float pidder(PID *props, float input, float dt);
 
 #endif
