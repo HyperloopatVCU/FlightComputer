2080e9544aa95e34e9606648f1f6a5d014bf1444
Nabbo454 <33235324+Nabbo454@users.noreply.github.com>
2017-10-30 18:00:38 -0400

    Update pidinv.c

--- PIDDER/src/pidinv.c
+++ PIDDER/src/pidinv.c
@@ -1,13 +1,13 @@
 #include"pidinv.h"
 //(design this to be used atomically)
 
-struct pidprops prop_default = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+struct pidprops prop_default = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};	//  Define initial positions of default properties.
 
 double pidder(PID *props, double input, double dt) {
-	double de = props->sp - input;
-	props->igral += de * dt;
-	props->dtive = (de - props->pe) / dt;
-	double output = (props->Kp * de) + (props->Ki * props->igral) + (props->Kd * props->dtive);
-	props->pe = de;
+	double de = props->sp - input;		//  Specify "error".
+	props->igral += de * dt;		//  Produce next integral step.
+	props->dtive = (de - props->pe) / dt;	//  Produce current derivative step.
+	double output = (props->Kp * de) + (props->Ki * props->igral) + (props->Kd * props->dtive); //  Calculate PID output.
+	props->pe = de;		//  Update past 'error'.
 	return output;
 }
