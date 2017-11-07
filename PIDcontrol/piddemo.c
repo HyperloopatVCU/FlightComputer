4dc4a4ae0cc1708b0f3ea23dbc60c3be8af53463
John Naylor <jonaylor89@gmail.com>
2017-11-07 16:53:36 -0500

    Stuff was bothing me

--- PIDcontrol/piddemo.c
+++ PIDcontrol/piddemo.c
@@ -1,6 +1,6 @@
-#include"pidinv.h"	//  PID library
-#include<stdio.h>	//  printf
-#include<stdlib.h>	//  atof
+#include "pidinv.h"	//  PID library
+#include <stdio.h>	//  printf
+#include <stdlib.h>	//  atof
 
 float testfunc(float x) {
 	return 0.468*x;			// testing function.
@@ -9,15 +9,15 @@ float testfunc(float x) {
 int main(int argc, char* argv[]) {
 	if (argc < 6)
 		return -1;
-	
+
 	PID props = prop_default;	//  Initialize PID properties to zero.
 	props.Kp = atof(argv[1]);	//  Receive command line args for PID constants, setpoint, and num of iterations.
 	props.Ki = atof(argv[2]);	//  !! no input protection !!
 	props.Kd = atof(argv[3]);
 	props.sp = atof(argv[4]);
-	
+
 	int samples = atoi(argv[5]);
-	
+
 	float x = 0.0, dt = 0.1;	//  Initial positions.
 					//  Print data to stdout in SC (UNIX spreadsheet program) format
 	printf("format A 11 6 0\nformat B 11 6 0\nlabel A0 = \"time\"\nlabel B0 = \"samples\"\n");
