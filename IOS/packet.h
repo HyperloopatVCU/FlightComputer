e50dfa457c4a6029284151c2c989ca79e941ebdd
John Naylor <jonaylor89@gmail.com>
2018-01-09 19:53:57 -0500

    Adding packet.h and practicing neural networks

--- IOS/packet.h
+++ IOS/packet.h
@@ -2,9 +2,8 @@
 
 typedef struct ator_t{
     time_t timestamp,
-    int8_t areacode,
-    int8_t typeID,
-    int8_t sensorID,
-    int8_t data
+    uint8_t typeID,
+    uint8_t sensorID,
+    uint8_t data
 }ator_t
     
