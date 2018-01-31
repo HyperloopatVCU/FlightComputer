35f726efea475131605acea4a13efb6c07474b2a
John Naylor <jonaylor89@gmail.com>
2018-01-30 22:27:48 -0500

    Added statemachine backbone

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
+from statemachine.statemachine import MainSM
+
 def main():
-    pass
+    SM = MainSM()
+
+    while True:
+        try:
+            SM.update()
+        except KeyboardInterrupt as e:
+            break;
+    
+    print("\n\n[+] Flight Sequence Finished")
+
 
 if __name__ == "__main__":
     main()
---
+++ statemachine/statemachine.py
@@ -0,0 +1,22 @@
+
+class MainSM(object):
+
+    def __init__(self):
+
+        self.states = {
+                    "cold" : "cold",
+                    "warm" : "warm",
+                    "hot!" : "hot!"
+                    }
+
+        self.state = self.states["cold"]
+
+
+    def update(self):
+        '''
+        TODO: Logic to determine state!
+        '''
+
+        pass
+
+
