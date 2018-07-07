03ca8b86dc28ad153920e835547310a089a7bc6d
John Naylor <jonaylor89@gmail.com>
2018-07-07 12:38:22 -0400

    New data_processing skeleton

---
+++ Communication/data_processing.py
@@ -0,0 +1,15 @@
+
+class Data_Processing(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        while True:
+            self.update()
+
+    def update(self):
+        """
+        Do the averaging and deviation checking in this function
+        """
+        pass
