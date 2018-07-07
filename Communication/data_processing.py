ec85b16b84f160c68e8b780469ed3ff64b140eee
John Naylor <jonaylor89@gmail.com>
2018-07-07 12:53:31 -0400

    More data processing module stuff

--- Communication/data_processing.py
+++ Communication/data_processing.py
@@ -1,8 +1,17 @@
 
+import logging
+from configparser import ConfigParser
+
 class Data_Processing(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, tcp):
+        self.logger = logging.getLogger('COMM')
+        self.logger.info("[+] Initializing Data Processor")
+
+        self.config = ConfigParser()
+        self.config.read('config.ini')
+
+        self.frame_rate = self.config['DataProc'].getint('frame_rate')
 
     def run(self):
         while True:
