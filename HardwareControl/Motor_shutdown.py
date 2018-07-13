f5991fb1659ae65477f1de74ad1ea50bf73e9db8
Cat <jonathan.b.odell@gmail.com>
2018-07-12 23:14:21 -0400

    Jay's files

---
+++ HardwareControl/Motor_shutdown.py
@@ -0,0 +1,17 @@
+import motor.py as motor
+import rpi.GPIO as GPIO
+
+cont_pull_in_volt = int("volt",0x4655_08)
+
+class Motor_shutdown:
+    from motor import _safety:
+    key_switch = 09
+    from Allocation import 27 as backward switch:
+    def voltage(self):
+        if cont_pull_in_volt < 12:
+        key_switch = False
+        forward_switch = False
+        print("Motor Start")
+        else:
+        print("Motor dead")
+
